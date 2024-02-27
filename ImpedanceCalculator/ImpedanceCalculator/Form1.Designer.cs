f26a35c6f5b7a7407f29d7ecfeb821b5db7df898
Hoang Duong <hkdng97it@gmail.com>
2024-02-27 16:46:54 +0100

    update images

--- ImpedanceCalculator/ImpedanceCalculator/Form1.Designer.cs
+++ ImpedanceCalculator/ImpedanceCalculator/Form1.Designer.cs
@@ -81,7 +81,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
             label2.Location = new Point(12, 9);
             label2.Name = "label2";
             label2.Size = new Size(197, 37);
