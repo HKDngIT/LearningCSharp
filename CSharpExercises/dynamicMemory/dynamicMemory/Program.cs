bc4065f6e3f5bf00f1ceb50d816dde9f92e2c50d
Hoang Duong <hkdng97it@gmail.com>
2024-03-08 10:45:24 +0100

    added exercise for dynamic memory exercise 1

--- CSharpExercises/dynamicMemory/dynamicMemory/Program.cs
+++ CSharpExercises/dynamicMemory/dynamicMemory/Program.cs
@@ -1,2 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+/* This function is just used to run the exercises
+ */
+execute();
+void execute()
+{
+    exercise1();
+}
+
+/* Exercise 1
+ * Copy an Array with doubled array elements
+ */
+
+unsafe void exercise1()
+{
+    int[] iArray1 = { 1, 2, 3 };
+    int* iArray2 = twiceret(iArray1, iArray1.Length);
+
+    // Print the result
+    for(int i = 0; i < 3; i++)
+    {
+        Console.Write(iArray1[i] + " ");
+    }
+    Console.Write('\n');
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write(iArray2[i] + " ");
+    }
+    Console.Write('\n');
+
+    // Free the allocated memory
+    Marshal.FreeHGlobal((IntPtr)iArray2);
+}
+
+
+unsafe int* twiceret(int[] arr, int length)
+{
+    // Allocate the unmanaged memory for an int array of 3 integers
+    IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(int)) * 3);
+
+    // Access and use the allocated memory
+    int* twice = (int*)ptr.ToPointer();
+    for (int index = 0; index < length; index++)
+    {
+        twice[index] = arr[index] * 2;
+    }
+    return twice;
+}
--- CSharpExercises/dynamicMemory/dynamicMemory/obj/Debug/net8.0/dynamicMemory.AssemblyInfo.cs
+++ CSharpExercises/dynamicMemory/dynamicMemory/obj/Debug/net8.0/dynamicMemory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dynamicMemory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b703c1390854f55ac2b583e3a932b7b12f9afdbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717958d490909ada8f2bae299f762644340bdaf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dynamicMemory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dynamicMemory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
