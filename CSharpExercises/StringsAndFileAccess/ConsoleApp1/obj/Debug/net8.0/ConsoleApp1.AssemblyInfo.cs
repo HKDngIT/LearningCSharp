57569962306e4eeaba1f3ab4a4b5a308d8b3a46c
Hoang Duong <hkdng97it@gmail.com>
2024-03-13 18:55:38 +0100

    test commit from linux

--- CSharpExercises/StringsAndFileAccess/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ CSharpExercises/StringsAndFileAccess/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951e22898d8d3ef6d18d3df32589ab74044fabf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f533126cb752adcdfb4c143739467fb68f71fbc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
