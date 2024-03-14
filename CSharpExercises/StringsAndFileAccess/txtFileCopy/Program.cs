a2f66cebcc45301d77d39a7e19bfe6da4b0f0613
Hoang Duong <hkdng97it@gmail.com>
2024-03-14 19:44:58 +0100

    added comments for clearer understanding

--- CSharpExercises/StringsAndFileAccess/txtFileCopy/Program.cs
+++ CSharpExercises/StringsAndFileAccess/txtFileCopy/Program.cs
@@ -9,11 +9,17 @@ void main() {
 }
 
 void example(){
+    // Specify the path of the text file
     string filepath = "CopyThis.txt";
+
+    // Check if the file exists
     if(File.Exists(filepath)) {
         Console.WriteLine("File exists!");
+
+        // Read all lines from the file
         string[] lines = File.ReadAllLines(filepath);
 
+        // Display each line
         Console.WriteLine("Content of CopyThis.txt");
         foreach(string line in lines) {
             Console.WriteLine(line);
@@ -23,9 +29,13 @@ void example(){
         Console.WriteLine("File not found!");
     }
 
+    // Specify the path of the text file
     string filePathToWrite = "CopyIntoThis.txt";
+
+    // Data to be written
     string[] data = { "Line 1", "Line 2", "Line 3"};
 
+    // Write data into the file
     File.WriteAllLines(filePathToWrite, data);
     Console.WriteLine("Data has been written onto CopyIntoThis.txt");   
 }
