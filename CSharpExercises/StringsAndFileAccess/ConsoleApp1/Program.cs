9aa42e76a7ae5fe1227e90327aa5e8a622ca338f
Hoang Duong <hkdng97it@gmail.com>
2024-03-14 13:11:49 +0100

    very simple console calculator

--- CSharpExercises/StringsAndFileAccess/ConsoleApp1/Program.cs
+++ CSharpExercises/StringsAndFileAccess/ConsoleApp1/Program.cs
@@ -3,5 +3,49 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        // Variables
+        int m_iNum1;
+        int m_INum2;
+        string m_input;
+        int m_result = 0;
+        
+        bool running = true;
+
+        while(running) {
+            Console.Write("Term: ");
+            m_input = Convert.ToString(Console.ReadLine());
+
+            if(m_input == "quit") {
+                running = false;
+                Console.WriteLine("Quiting....");
+            } else {
+
+                string[] parts = m_input.Split(' ');
+    
+                m_iNum1 = int.Parse(parts[0]);
+                char op = char.Parse(parts[1]);
+                m_INum2 = int.Parse(parts[2]);
+    
+                switch(op) 
+                {
+                    case '+': 
+                        m_result = m_iNum1 + m_INum2;
+                        break;
+    
+                    case '-':
+                        m_result = m_iNum1 - m_INum2;
+                        break;
+                    
+                    case '*':
+                        m_result = m_iNum1 * m_INum2;
+                        break;
+    
+                    case '/':
+                        m_result = m_iNum1 + m_INum2;
+                        break;
+                }
+                Console.WriteLine($"{m_input} = {m_result}");
+            }
+        }
     }
 }
--- CSharpExercises/StringsAndFileAccess/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ CSharpExercises/StringsAndFileAccess/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f533126cb752adcdfb4c143739467fb68f71fbc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57569962306e4eeaba1f3ab4a4b5a308d8b3a46c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
