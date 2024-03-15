d409591dc9f672d849c0ac91a507536802968674
Hoang Duong <hkdng97it@gmail.com>
2024-03-15 14:59:35 +0100

    added male name list

--- CSharpExercises/StringsAndFileAccess/GreetingAssignment/Program.cs
+++ CSharpExercises/StringsAndFileAccess/GreetingAssignment/Program.cs
@@ -6,7 +6,7 @@ main();
 /* 1    Load the txt files for male and female names
 */
 void main() {
-    // Specify the path to the text file
+    // Specify the path to the text file for female names
     string m_filepath = "txtFiles/fNames.txt";
 
     // Read all lines from the file and fill the array with female names
@@ -16,4 +16,17 @@ void main() {
     foreach(string line in m_fNames) {
         Console.WriteLine(line);
     }
+    Console.WriteLine($"m_fNames.length = {m_fNames.Length}");
+
+    // Specify the path to the text file for male names
+    m_filepath = "txtFiles/mNames.txt";
+
+    // Read all lines from the file and fill the array with male names
+    string[] m_mNames = File.ReadAllLines(m_filepath);
+    
+    // Test print all male Names
+    foreach(string line in m_mNames) {
+        Console.WriteLine(line);
+    }
+    Console.WriteLine($"m_mNames.length = {m_mNames.Length}");
 }
--- CSharpExercises/StringsAndFileAccess/GreetingAssignment/obj/Debug/net8.0/GreetingAssignment.AssemblyInfo.cs
+++ CSharpExercises/StringsAndFileAccess/GreetingAssignment/obj/Debug/net8.0/GreetingAssignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreetingAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+084887d22ef4de9e6fecadee2db88ae2d1416a44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077150cc8f4f4e0475087c513193d15d5ec27286")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreetingAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreetingAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
